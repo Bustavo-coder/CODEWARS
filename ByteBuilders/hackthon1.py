fdc23ce430960558f7769cb0d542859f53b9583a
Bustavo-coder <abdbasitegberongbe@gmail.com>
2025-10-15 07:19:01 +0100

    aa

--- ByteBuilders/hackthon1.py
+++ ByteBuilders/hackthon1.py
@@ -1,22 +1,12 @@
-def function(iterable):
-	if any(!element.isalphanum()for element in iterable):
-		raise ValueError("Can Only take in integers")
-	length_list = list()
-	new_text = iterable.split(" ")
-	for elements in new_text:
-		length_list.append(len(elements))
-		length_list.sort()	
-	if len(elements) == max(length_list):
-		return  elements
+def function(string):
+	string = string.split(" ")
+	length = [len(words) for words in string]
+	highest = max(length)
+	for words in string:if len(words) == highest :return words
+	
+print(function("The Quick brown fox jumped"))
 
-function("The Quick brown fox jumped")
-
-
-
-
-def check(string):
-	for elements in enumerate(strings):
-		if el
+		
 
 	
 
